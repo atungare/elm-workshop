bf60ab3b0166177c0339cc2558aab1bfb3ea58c2
atungare <ajay.tungare@gmail.com>
2016-05-01 19:24:49 -0700

    wip part 6

--- part6/ElmHub.elm
+++ part6/ElmHub.elm
@@ -23,8 +23,6 @@ searchFeed query =
         ++ query
         ++ "+language:elm&sort=stars&order=desc"
 
-    -- TODO define task as:
-    --
     -- task = performAction argument1 argument2 argument3
     --
     -- Use these "ingredients" to give `performAction` the arguments it needs:
@@ -37,12 +35,12 @@ searchFeed query =
     --
     -- Hint: http://package.elm-lang.org/packages/evancz/elm-http/3.0.0/Http#get
     task =
-      "TODO performAction ..."
+      performAction
+        HandleSearchResponse
+        HandleSearchError
+        (Http.get responseDecoder url)
   in
-    -- TODO replace this `Effects.none` with a call to:
-    --
-    -- Effects.task task
-    Effects.none
+    Effects.task task
 
 
 {-| Note: this will be a standard function in the next release of Elm.
